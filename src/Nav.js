c8d66c4c6b01308ef4a9a9e1f5e6cf5f09b03f07
Mohideen Abdul Katheer <MohideenAbdulKatheer.MohamedAmsa@ig.com>
2022-02-18 11:34:09 +0530

    Project Setup

---
+++ src/Nav.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Nav = () => (
+    <nav>
+        <h1>Logo</h1>
+        <ul>
+            <li>Home</li>
+            <li>Services</li>
+            <li>About</li>
+        </ul>
+    </nav>
+);
+
+export default Nav;
